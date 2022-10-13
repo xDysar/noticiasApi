6da262540c4cddcde68dde41acffa3d2f1a1faed
xDysar <lucasbnicolas@gmail.com>
2022-10-13 16:47:18 -0300

    Version final

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -6,36 +6,56 @@ const NoticiasContext = createContext()
 const NoticiasProvider = ({ children }) => {
   const [categoria, setCategoria] = useState('general')
   const [noticias, setNoticias] = useState([])
+  const [pagina, setPagina] = useState(1)
+  const [totalNoticias, setTotalNoticias] = useState(0)
 
   useEffect(() => {
     const consultarAPI = async () => {
       const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${
         import.meta.env.VITE_API_KEY
       }`;
-
-      const {data} = await axios(url)
-      setNoticias(data.articles);
+      const { data } = await axios(url)
+      setNoticias(data.articles)
+      setTotalNoticias(data.totalResults);
+      setPagina(1)
     }
     consultarAPI()
   }, [categoria])
-  
 
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&page=${pagina}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const { data } = await axios(url);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+    };
+    consultarAPI();
+  }, [pagina]);
 
   const handleChangeCategoria = e => {
     setCategoria(e.target.value)
   }
+
+  const handleChangePagina = (e, valor) => {
+    setPagina(valor)
+  };
 
   return (
     <NoticiasContext.Provider
       value={{
         categoria,
         handleChangeCategoria,
-        noticias
+        noticias,
+        totalNoticias,
+        handleChangePagina,
+        pagina
       }}
     >
       {children}
     </NoticiasContext.Provider>
-  )
+  );
 }
 
 export {
