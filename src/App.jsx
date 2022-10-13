413c4a56bf785ee1da8587832f3f5c98e7836d0c
xDysar <lucasbnicolas@gmail.com>
2022-10-13 03:50:58 -0300

    V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { Container, Grid, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
+import { NoticiasProvider } from "./context/NoticiasProvider";
 
 function App() {
 
   return (
-    <Container>
-      
-    </Container>
-  )
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component='h1' variant="h3">
+            Buscador de Noticias
+          </Typography>
+        </header>
+
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+      </Container>
+    </NoticiasProvider>
+  );
 }
 
 export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,47 @@
+import { FormControl, InputLabel, Select, MenuItem, Button, Box } from '@mui/material'
+import useNoticias from '../hooks/useNoticias';
+
+
+const CATEGORIAS = [
+  { value: "general", label: "General" },
+  { value: "business", label: "Negocios" },
+  { value: "entertainment", label: "Entretenimiento" },
+  { value: "health", label: "Salud" },
+  { value: "science", label: "Ciencia" },
+  { value: "sports", label: "Deportes" },
+  { value: "technology", label: "Tecnología" },
+];
+
+const Formulario = () => {
+  const { categoria, handleChangeCategoria } = useNoticias();
+
+
+  return (
+    <form>
+      <FormControl fullWidth>
+        <InputLabel>Categoria</InputLabel>
+        <Select
+          label='Categoria'
+          onChange={handleChangeCategoria}
+          value={categoria}
+        >
+          {CATEGORIAS.map(categoria => (
+            <MenuItem
+              key={categoria.value}
+              value={categoria.value}
+            >
+              {categoria.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <Box sx={{marginTop: 2}}>
+          <Button variant='contained' color='primary' fullWidth>
+            Buscar noticias
+          </Button>
+        </Box>
+      </FormControl>
+    </form>
+  )
+}
+
+export default Formulario
